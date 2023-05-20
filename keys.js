3fcf6c406f51d19f37c2ed3672c02e371b7547fc
sadkris <13920323+sadkris@users.noreply.github.com>
2023-05-20 17:29:36 -0400

    Hopefully the last fix

--- keys.js
+++ keys.js
@@ -1,34 +1,32 @@
-fetch("keys.json")
-   .then(response => response.text())
-   .then((response) => {
-       console.log(response)
-       const keys = JSON.parse(response)
-   })
-   .catch(err => console.log(err))
+const generateKeyBtn = document.querySelector("#generate-key-btn");
+const keyInfoDiv = document.querySelector("#key-info");
+const understandCheckbox = document.querySelector("#understand-checkbox");
 
-   const generateKeyBtn = document.querySelector("#generate-key-btn");
-   const keyInfoDiv = document.querySelector("#key-info");
-   const understandCheckbox = document.querySelector("#understand-checkbox");
+function generateKey() {
+  const keys = fetch("keys.json")
+    .then(response => response.text())
+    .then((response) => {
+      return JSON.parse(response)
+    })
+    .catch(err => console.log(err))
+  if (!understandCheckbox.checked) {
+    keyInfoDiv.innerHTML = "Please check the checkbox to confirm that you understand and agree to only redeem one key.";
+    return;
+  }
 
-   function generateKey() {
-     if (!understandCheckbox.checked) {
-       keyInfoDiv.innerHTML = "Please check the checkbox to confirm that you understand and agree to only redeem one key.";
-       return;
-     }
+  const randomIndex = Math.floor(Math.random() * (14));
+  const keyTitleArray = keys[randomIndex];
 
-     const randomIndex = Math.floor(Math.random() * (14));
-     const keyTitleArray = keys[randomIndex];
+  const title = keyTitleArray[0];
+  const key = keyTitleArray[1];
 
-     const title = keyTitleArray[0];
-     const key = keyTitleArray[1];
+  const redeemUrl = `https://store.steampowered.com/account/registerkey?key=${key}`;
 
-     const redeemUrl = `https://store.steampowered.com/account/registerkey?key=${key}`;
+  keyInfoDiv.innerHTML = `
+    <p><strong>Title:</strong> ${title}</p>
+    <p><strong>Key:</strong> ${key}</p>
+    <p><a href="${redeemUrl}" target="_blank">Click here to redeem the game</a></p>
+  `;
+}
 
-     keyInfoDiv.innerHTML = `
-       <p><strong>Title:</strong> ${title}</p>
-       <p><strong>Key:</strong> ${key}</p>
-       <p><a href="${redeemUrl}" target="_blank">Click here to redeem the game</a></p>
-     `;
-   }
-
-   generateKeyBtn.addEventListener("click", generateKey);
+generateKeyBtn.addEventListener("click", generateKey);
