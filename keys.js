5e85bf97cd21fb0db223e8885ec2a7dcaf64c5eb
sadkris <13920323+sadkris@users.noreply.github.com>
2023-05-20 17:02:48 -0400

    Adding the final changes to the script

--- keys.js
+++ keys.js
@@ -1,6 +1,33 @@
 fetch("keys.json")
    .then(response => response.text())
    .then((response) => {
-       console.log(response)
+       const keys = JSON.parse(response.text);
    })
    .catch(err => console.log(err))
+
+   const generateKeyBtn = document.querySelector("#generate-key-btn");
+   const keyInfoDiv = document.querySelector("#key-info");
+   const understandCheckbox = document.querySelector("#understand-checkbox");
+
+   function generateKey() {
+     if (!understandCheckbox.checked) {
+       keyInfoDiv.innerHTML = "Please check the checkbox to confirm that you understand and agree to only redeem one key.";
+       return;
+     }
+
+     const randomIndex = Math.floor(Math.random() * (14));
+     const keyTitleArray = keys[randomIndex];
+
+     const title = keyTitleArray[0];
+     const key = keyTitleArray[1];
+
+     const redeemUrl = `https://store.steampowered.com/account/registerkey?key=${key}`;
+
+     keyInfoDiv.innerHTML = `
+       <p><strong>Title:</strong> ${title}</p>
+       <p><strong>Key:</strong> ${key}</p>
+       <p><a href="${redeemUrl}" target="_blank">Click here to redeem the game</a></p>
+     `;
+   }
+
+   generateKeyBtn.addEventListener("click", generateKey);
