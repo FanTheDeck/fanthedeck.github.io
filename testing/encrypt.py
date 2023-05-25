46762816ed91df605c79872c6a5893a290ea6ddc
sadkris <13920323+sadkris@users.noreply.github.com>
2023-05-25 12:13:40 -0400

    Fixing multi-line input

--- testing/encrypt.py
+++ testing/encrypt.py
@@ -2,13 +2,24 @@ import base64
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad
 import json
+import sys
 
 key = input("Enter 16 byte long key: ").encode()
 iv = b''
 for i in [21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36]:
     iv += i.to_bytes(1, 'big')
 
-keyList = json.loads(input("Enter list of keys: "))
+print("Enter list of keys: ")
+
+keyList = []
+
+while True:
+    line = sys.stdin.readline()
+    if line.strip() == '':
+        break
+    keyList.append(line.strip())
+
+keyList = json.loads(''.join(keyList))
 
 def encrypt(steamKey):
     cipher = AES.new(key, AES.MODE_CBC, iv)
